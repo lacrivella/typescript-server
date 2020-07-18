57f4f88134f6092543168eac34fbb3eac8a47499
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-07-17 20:39:36 -0700

    tested rebuild

--- build/controllers/decorators/routes.js
+++ build/controllers/decorators/routes.js
@@ -1,9 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
-function get(path) {
-    return function (target, key, desc) {
-        Reflect.defineMetadata('path', path, target, key);
+function routeBinder(method) {
+    return function (path) {
+        return function (target, key, desc) {
+            Reflect.defineMetadata('path', path, target, key);
+            Reflect.defineMetadata('method', method, target, key);
+        };
     };
 }
-exports.get = get;
+exports.get = routeBinder('get');
+exports.post = routeBinder('post');
+exports.put = routeBinder('put');
+exports.patch = routeBinder('patch');
+exports.del = routeBinder('delete');

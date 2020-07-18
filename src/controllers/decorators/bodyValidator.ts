78ca7029cbe239d2ff6809ee031110ec7df6131c
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-07-18 13:58:21 -0700

    wired up body validator stuff

--- src/controllers/decorators/bodyValidator.ts
+++ src/controllers/decorators/bodyValidator.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import { MetadataKeys } from './MetadataKeys';
 
-export function bodyValidator() {
-  
+export function bodyValidator(...keys: string[]) {
+  return function (target: any, key: string, desc: PropertyDescriptor) {
+    Reflect.defineMetadata(MetadataKeys.validator, keys, target, key);
+  };
 }
--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,7 +1,25 @@
 import 'reflect-metadata';
+import { Request, Response, RequestHandler, NextFunction } from 'express';
 import { AppRouter } from '../../AppRouter';
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
+
+function bodyValidators(keys: string): RequestHandler {
+  return function (req: Request, res: Response, next: NextFunction) {
+    if (!req.body) {
+      res.status(422).send('Invalid Request');
+      return;
+    }
+
+    for (let key of keys) {
+      if (!req.body[key]) {
+        res.status(422).send('Invalid Request');
+        return;
+      }
+    }
+    next();
+  };
+}
 
 export function controller(routePrefix: string) {
   return function (target: Function) {
@@ -23,8 +41,19 @@ export function controller(routePrefix: string) {
         Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) ||
         [];
 
+      const requiredBodyProps =
+        Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) ||
+        [];
+
+      const validator = bodyValidators(requiredBodyProps);
+
       if (path) {
-        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
+        router[method](
+          `${routePrefix}${path}`,
+          ...middlewares,
+          validator,
+          routeHandler
+        );
       }
     }
   };

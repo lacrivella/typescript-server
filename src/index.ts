b5dcf5695575762be71d96475f7d00a2b08b3568
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-07-18 14:38:42 -0700

    removed anything related to loginroutes and changed it to root controller

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import express, { Request, Response } from 'express';
-import { router } from './routes/loginRoutes';
 import bodyParse from 'body-parser';
 import cookieSession from 'cookie-session';
 import { AppRouter } from './AppRouter';
 import './controllers/LoginController';
+import './controllers/RootController';
 
 const app = express();
 
 app.use(bodyParse.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['fake string'] }));
-app.use(router);
 app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
